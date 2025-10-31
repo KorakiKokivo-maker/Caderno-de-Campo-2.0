f7fa41b65a5bff3fce82d9b497c68ec7b28b78f9
KorakiKokivo-maker <jefersonbitellojunior@gmail.com>
2025-10-31 19:57:46 -0300

    Update database.ts

--- database.ts
+++ database.ts
@@ -1,5 +1,5 @@
-export { addUsuario as addUser, getUsuarios as getUsers, updateUsuario as updateUser } from './mockDatabase';
-export { addPropriedade, getPropriedadesByUsuario, updatePropriedade, deletePropriedade } from './mockDatabase';
-export { addColheita, getColheitasBySafra, updateColheita, deleteColheita } from './mockDatabase';
-export { addBenfeitoria, getBenfeitoriasByUsuario, updateBenfeitoria, deleteBenfeitoria } from './mockDatabase';
-export { addSafra, getSafrasByUsuario, updateSafra, deleteSafra } from './mockDatabase';
+export { addUsuario as addUser, getUsuarios as getUsers, updateUsuario as updateUser } from './services/mockDatabase';
+export { addPropriedade, getPropriedadesByUsuario, updatePropriedade, deletePropriedade } from './services/mockDatabase';
+export { addColheita, getColheitasBySafra, updateColheita, deleteColheita } from './services/mockDatabase';
+export { addBenfeitoria, getBenfeitoriasByUsuario, updateBenfeitoria, deleteBenfeitoria } from './services/mockDatabase';
+export { addSafra, getSafrasByUsuario, updateSafra, deleteSafra } from './services/mockDatabase';
